cbec47f529bca5c787fb4373fe2a0450c04b6e22
L1ght <2504082957@qq.com>
2021-09-25 06:50:21 -0700

    Add print.c

--- Lab1/lab1/lib/print.c
+++ Lab1/lab1/lib/print.c
@@ -1,10 +1,36 @@
 #include "print.h"
 #include "sbi.h"
 
+
 void puts(char *s) {
-    // unimplemented
+    // implemented
+    int i = 0;
+    while(s[i]){
+        uint64 _output = s[i];
+        sbi_ecall(0x1, 0x0, _output, 0, 0, 0, 0, 0);
+        i++;
+    }
 }
 
+
 void puti(int x) {
-    // unimplemented
+    // implemented
+    unsigned long long cyc;
+    int _x = x;
+    int length = 0;
+    cyc = 1;
+    while(cyc <= x){
+        cyc *= 10;
+        length++;
+    }
+    cyc /= 10;
+
+    while(cyc){
+        int current = _x / cyc;
+        _x = _x % cyc;
+        cyc /= 10;
+        sbi_ecall(0x1, 0x0, (current + 0x30), 0, 0, 0, 0, 0);
+
+    }
+
 }
