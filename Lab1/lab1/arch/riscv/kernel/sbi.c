bd6c551be70cf6575c13e6cb2a74bca08a60e561
L1ght <2504082957@qq.com>
2021-10-14 04:59:16 -0700

    Add the specific regs in __asm__

--- Lab1/lab1/arch/riscv/kernel/sbi.c
+++ Lab1/lab1/arch/riscv/kernel/sbi.c
@@ -12,12 +12,12 @@ struct sbiret sbi_ecall(int ext, int fid, uint64 arg0,
 	__asm__ volatile (
 		"add a7, x0, %[ext]\n"
 		"add a6, x0, %[fid]\n"
-		"add a0, x0, %[arg0]\n"
-		"add a1, x0, %[arg1]\n"
-		"add a2, x0, %[arg2]\n"
-		"add a3, x0, %[arg3]\n"
-		"add a4, x0, %[arg4]\n"
 		"add a5, x0, %[arg5]\n"
+		"add a4, x0, %[arg4]\n"
+		"add a3, x0, %[arg3]\n"
+		"add a2, x0, %[arg2]\n"
+		"add a1, x0, %[arg1]\n"
+		"add a0, x0, %[arg0]\n"
 		"ecall\n"
 		"add %[value], x0, a1\n"
 		"add %[error], x0, a0\n"
@@ -26,7 +26,7 @@ struct sbiret sbi_ecall(int ext, int fid, uint64 arg0,
 		:[ext] "r" (ext), [fid] "r" (fid), 
 		[arg0] "r" (arg0), [arg1] "r" (arg1), [arg2] "r" (arg2), 
 		[arg3] "r" (arg3), [arg4] "r" (arg4), [arg5] "r" (arg5)
-		:"memory"
+		:"memory","a0","a1","a2","a3","a4","a5"
 	);
 
 	struct sbiret _ret;
