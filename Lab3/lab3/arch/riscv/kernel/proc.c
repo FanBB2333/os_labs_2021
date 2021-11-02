dd4c2699135746bd173dde5ee1fddbb3a26b924b
l1ght <2504082957@qq.com>
2021-11-02 09:30:53 +0800

    partially finish 4.3.1

--- Lab3/lab3/arch/riscv/kernel/proc.c
+++ Lab3/lab3/arch/riscv/kernel/proc.c
@@ -14,6 +14,14 @@ void task_init() {
     // 5. 将 current 和 task[0] 指向 idle
 
     /* YOUR CODE HERE */
+    idle = kalloc();
+    idle->state = TASK_RUNNING;
+    idle->counter = 0;
+    idle->priority = 0;
+    idle->pid = 0;    
+    current = idle;
+    task[0] = idle;
+
 
     // 1. 参考 idle 的设置, 为 task[1] ~ task[NR_TASKS - 1] 进行初始化
     // 2. 其中每个线程的 state 为 TASK_RUNNING, counter 为 0, priority 使用 rand() 来设置, pid 为该线程在线程数组中的下标。
@@ -22,5 +30,31 @@ void task_init() {
 
     /* YOUR CODE HERE */
 
+    for(int i = 1; i < NR_TASKS; i++){
+        task[i] = kalloc();
+        task[i]->state = TASK_RUNNING;
+        task[i]->counter = 0;
+        task[i]->priority = rand();
+        task[i]->pid = i; 
+        task[i].thread.ra = (uint64)
+        task[i].thread.sp = (uint64)(&(task[i]) + PGSIZE);
+
+    }
+
     printk("...proc_init done!\n");
+}
+
+// arch/riscv/kernel/proc.c
+
+void dummy() {
+    uint64 MOD = 1000000007;
+    uint64 auto_inc_local_var = 0;
+    int last_counter = -1;
+    while(1) {
+        if (last_counter == -1 || current->counter != last_counter) {
+            last_counter = current->counter;
+            auto_inc_local_var = (auto_inc_local_var + 1) % MOD;
+            printk("[PID = %d] is running. auto_inc_local_var = %d\n", current->pid, auto_inc_local_var);
+        }
+    }
 }
