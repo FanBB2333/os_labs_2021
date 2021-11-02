acaf607a6e73950419785bc805973f7815423600
L1ght <2504082957@qq.com>
2021-11-02 05:42:05 -0700

    add print out

--- Lab3/lab3/init/main.c
+++ Lab3/lab3/init/main.c
@@ -1,12 +1,13 @@
 #include "printk.h"
 #include "sbi.h"
+#include "proc.h"
 
 extern void test();
 
 int start_kernel() {
     printk("2021");
     printk(" Hello RISC-V\n");
-
+    dummy();
     test(); // DO NOT DELETE !!!
 
 	return 0;
