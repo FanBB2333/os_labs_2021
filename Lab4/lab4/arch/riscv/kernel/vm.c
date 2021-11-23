8faaa1f4c6bceccd63c3b0beccfefd6ccdb175ba
L1ght <2504082957@qq.com>
2021-11-23 07:31:38 -0800

    add setup_vm_final

--- Lab4/lab4/arch/riscv/kernel/vm.c
+++ Lab4/lab4/arch/riscv/kernel/vm.c
@@ -1,5 +1,11 @@
+#include "defs.h"
+#include "mm.h"
+#include "string.h"
+
 // arch/riscv/kernel/vm.c
 
+extern void relocate(long OFFSET);
+void create_mapping(uint64 *pgtbl, uint64 va, uint64 pa, uint64 sz, int perm);
 /* early_pgtbl: 用于 setup_vm 进行 1GB 的 映射。 */
 unsigned long  early_pgtbl[512] __attribute__((__aligned__(0x1000)));
 
@@ -29,7 +35,48 @@ void setup_vm(void) {
     pte = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3) | (getppn(0x80000000, 2) << 30);
     early_pgtbl[getvpn(0x80000000, 2)] = pte; // PA == VA
     early_pgtbl[getvpn(0xffffffe000000000, 2)] = pte; // PA + PV2VA_OFFSET == VA
+    relocate((long)PA2VA_OFFSET);
     
+}
 
-    
+// arch/riscv/kernel/vm.c 
+
+/* swapper_pg_dir: kernel pagetable 根目录， 在 setup_vm_final 进行映射。 */
+unsigned long  swapper_pg_dir[512] __attribute__((__aligned__(0x1000)));
+
+void setup_vm_final(void) {
+    memset(swapper_pg_dir, 0x0, PGSIZE);
+
+    // No OpenSBI mapping required
+
+    // mapping kernel text X|-|R|V
+    create_mapping(...);
+
+    // mapping kernel rodata -|-|R|V
+    create_mapping(...);
+
+    // mapping other memory -|W|R|V
+    create_mapping(...);
+
+    // set satp with swapper_pg_dir
+
+    YOUR CODE HERE
+
+    // flush TLB
+    asm volatile("sfence.vma zero, zero");
+    return;
+}
+
+
+/* 创建多级页表映射关系 */
+void create_mapping(uint64 *pgtbl, uint64 va, uint64 pa, uint64 sz, int perm) {
+    /*
+    pgtbl 为根页表的基地址
+    va, pa 为需要映射的虚拟地址、物理地址
+    sz 为映射的大小
+    perm 为映射的读写权限
+
+    创建多级页表的时候可以使用 kalloc() 来获取一页作为页表目录
+    可以使用 V bit 来判断页表项是否存在
+    */
 }
