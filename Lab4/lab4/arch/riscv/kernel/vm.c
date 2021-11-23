47c69c4878d5715bf2cad7b5b70930a0d9a672df
L1ght <2504082957@qq.com>
2021-11-23 04:04:02 -0800

    fill in setup_vm

--- Lab4/lab4/arch/riscv/kernel/vm.c
+++ Lab4/lab4/arch/riscv/kernel/vm.c
@@ -3,6 +3,10 @@
 /* early_pgtbl: 用于 setup_vm 进行 1GB 的 映射。 */
 unsigned long  early_pgtbl[512] __attribute__((__aligned__(0x1000)));
 
+int getvpn(unsigned long va, int idx){
+    return ((va >> (12 + idx * 9)) & 0x1FF);
+}
+
 void setup_vm(void) {
     /* 
     1. 由于是进行 1GB 的映射 这里不需要使用多级页表 
@@ -13,6 +17,9 @@ void setup_vm(void) {
     3. Page Table Entry 的权限 V | R | W | X 位设置为 1
     */
     unsigned long va;
+    early_pgtbl[getvpn(0x80000000, 2)] = 0x80000000; // PA == VA
+    early_pgtbl[getvpn(0xffffffe000000000, 2)] = 0x80000000; // PA + PV2VA_OFFSET == VA
+    
 
     
 }
