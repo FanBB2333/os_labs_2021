b8c6441817263842e5e23b4713e3714105ec1fd8
L1ght <2504082957@qq.com>
2021-11-23 07:11:44 -0800

    upd main

--- Lab3/lab3/init/main.c
+++ Lab3/lab3/init/main.c
@@ -7,6 +7,7 @@ extern void test();
 int start_kernel() {
     printk("2021");
     printk(" Hello RISC-V\n");
+    printk("unsigned long : %d\n", sizeof(unsigned long));
     test(); // DO NOT DELETE !!!
 
 	return 0;
