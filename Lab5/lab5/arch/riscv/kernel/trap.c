1efb96380f9dfb78033113e94a0da3ae771ecde4
L1ght <2504082957@qq.com>
2021-12-08 08:22:33 -0800

    try to handle Environment call from U-mode

--- Lab5/lab5/arch/riscv/kernel/trap.c
+++ Lab5/lab5/arch/riscv/kernel/trap.c
@@ -6,8 +6,7 @@
 int dec2bit(unsigned long num, int index) {
     return (num>>(index-1)) & 1;
 }
-
-void trap_handler(unsigned long scause, unsigned long sepc) {
+void trap_handler(uint64_t scause, uint64_t sepc, struct pt_regs *regs) {
     // 通过 `scause` 判断trap类型
     // 如果是interrupt 判断是否是timer interrupt
     // 如果是timer interrupt 则打印输出相关信息, 并通过 `clock_set_next_event()` 设置下一次时钟终端
@@ -20,9 +19,20 @@ void trap_handler(unsigned long scause, unsigned long sepc) {
     if(interrupt == 1){
         unsigned long exception_code = scause - (1UL << 63); 
         if(exception_code == 5){
-            // printk("call do_timer\n");
             clock_set_next_event();
             do_timer();
         }
+    }
+
+    else if(interrupt == 0){
+        printk("exception: %d\n", scause);
+        unsigned long exception_code = scause - (1UL << 63); 
+        //Environment call from U-mode
+        if (exception_code == 8) {
+            printk("Environment call from U-mode\n");
+            // something else to do
+        }
+
+        
     }
 }
